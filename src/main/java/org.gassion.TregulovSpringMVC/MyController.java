f63129e329d75bb4ad6dae702bd29345ed29b2eb
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-03 22:20:57 +0300

    feat : add first-view

---
+++ src/main/java/org.gassion.TregulovSpringMVC/MyController.java
@@ -0,0 +1,14 @@
+package org.gassion.TregulovSpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+}
