6ff2d637838592731b5591c7fa68fd06dd388aa1
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-19 23:43:08 +0300

    fix :

---
+++ src/main/java/org/gassion/TregulovSpringMVC/config/SpringMVCDispatcher.java
@@ -0,0 +1,31 @@
+package org.gassion.TregulovSpringMVC.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
--- src/main/java/org/gassion/TregulovSpringMVC/controller/MainController.java
+++ src/main/java/org/gassion/TregulovSpringMVC/controller/MainController.java
@@ -1,14 +1,14 @@
-package org.gassion.TregulovSpringMVC;
+package org.gassion.TregulovSpringMVC.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyController {
-
-    @RequestMapping("/")
-    public String showFirstView() {
-        return "first-view";
+@RequestMapping("/new")
+public class MainController {
+    @GetMapping
+    public String getIndex() {
+        return "index";
     }
 }
